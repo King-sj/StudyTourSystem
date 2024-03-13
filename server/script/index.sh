fc12faea1fa2c9a2da7367cef660485fcc2be12d
King-sj <2022212702@bupt.cn>
2024-03-13 22:43:53 +0800

    update

---
+++ server/script/index.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+# Created by SongJian on 2024-03-13.
+# auto setup server shell script
+#
+. script/utils/index.sh
---
+++ server/script/utils/index.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Usage: warning "This is a warning message."
+
+info() {
+echo -e "\033[0;34m INFO: $1 \033[0m"
+}
+
+warning() {
+echo -e "\033[0;33m WARNING: $1 \033[0m"
+}
+
+notice() {
+echo -e "\033[0;32m NOTICE: $1 \033[0m"
+}
+
+error() {
+echo -e "\033[0;31m ERROR: $1 \033[0m"
+}
+
+bug() {
+echo -e "\033[0;31m BUG: $1 \033[0m"
+}

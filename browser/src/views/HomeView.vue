db4df16cacdd51894a7e37dd17a12f1d8613ee07
King-sj <2022212702@bupt.cn>
2024-03-18 10:03:15 +0800

    ✨ feat(browser): osm
    open streetmap offline test

--- browser/src/views/HomeView.vue
+++ browser/src/views/HomeView.vue
@@ -8,26 +8,32 @@ const options: MapOptions = {
   center: latLng(40.731253, -73.996139),
   zoom: 12,
 };
+const x = 0;
+const y = 0;
+const z = 0;
+axios.get(`http://localhost:8080/data/v3/#${z}/${x}/${y}.png`).then(
+  res => console.log(res)
+)
 onMounted(()=>{
   var mymap = map('map').setView([116.3512345449219, 39.96383372433723], 13);
-  tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  tileLayer('http://localhost:8080/data/v3/#{z}/{x}/{y}', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
   }).addTo(mymap);
+
   // 监听地图的点击事件
   mymap.on('click', function(e) {
       var lat = e.latlng.lat;
       var lng = e.latlng.lng;
       // 显示点击的坐标
       alert("你点击的坐标是: " + lat + ", " + lng);
-
       // 你可以在这里添加代码来使用地理编码服务查询更多信息，比如建筑名称
   });
 })
 const name = ref('北京邮电大学')
 const res = ref('result')
 watch(name, ()=>{
-  axios.get('https://photon.komoot.io/api/?q=柏林')
+  axios.get(`https://photon.komoot.io/api/?q=${name}`)
   .then(response => {
     console.log(response);
     return response.data
---
+++ server/maptiles/README.md
@@ -0,0 +1,4 @@
+npm install -g tileserver-gl-light
+
+tileserver-gl-light zurich_switzerland.mbtiles
+

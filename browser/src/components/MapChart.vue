ed13ca31b0a92e030bc12e42a9b1bdda2039e262
King-sj <2022212702@bupt.cn>
2024-04-02 13:36:34 +0800

    ✨ feat(browser): 地图显示
    配置本地瓦片服务器，使用leaflet实现地图显示

---
+++ browser/src/components/MapChart.vue
@@ -0,0 +1,46 @@
+<script lang="ts">
+// import { defineComponent, onMounted, ref } from 'vue';
+// import * as echarts from 'echarts/core';
+// import { GeoComponent, TooltipComponent } from 'echarts/components';
+// import { MapChart } from 'echarts/charts';
+// import { CanvasRenderer } from 'echarts/renderers';
+// // import ChinaMapData from './path/to/china.json'; // 确保你有中国地图的JSON
+
+// echarts.use([GeoComponent, TooltipComponent, MapChart, CanvasRenderer]);
+
+// const name = ref('MapChart');
+// const chart = ref<HTMLElement>();
+
+// onMounted(() => {
+//   const myChart = echarts.init(chart.value!);
+
+//   echarts.registerMap('china', ChinaMapData);
+
+//   myChart.setOption({
+//     tooltip: {
+//       trigger: 'item',
+//       formatter: '{b}'
+//     },
+//     series: [
+//       {
+//         name: '中国',
+//         type: 'map',
+//         map: 'china',
+//         label: {
+//           show: true
+//         },
+//         data: [
+//           // 示例数据
+//           { name: '北京', value: Math.round(Math.random() * 1000) },
+//           { name: '天津', value: Math.round(Math.random() * 1000) }
+//           // 更多省份数据...
+//         ]
+//       }
+//     ]
+//   });
+// });
+</script>
+
+<template>
+  <div ref="chart" style="width: 600px; height: 400px;"></div>
+</template>
--- browser/src/views/HomeView.vue
+++ browser/src/views/HomeView.vue
@@ -3,20 +3,13 @@ import { map, latLng, tileLayer } from "leaflet";
 import type {MapOptions} from "leaflet"
 import { ref, reactive, onMounted, watch } from "vue";
 import axios from "axios";
-
 const options: MapOptions = {
   center: latLng(40.731253, -73.996139),
   zoom: 12,
 };
-const x = 0;
-const y = 0;
-const z = 0;
-axios.get(`http://localhost:8080/data/v3/#${z}/${x}/${y}.png`).then(
-  res => console.log(res)
-)
 onMounted(()=>{
-  var mymap = map('map').setView([116.3512345449219, 39.96383372433723], 13);
-  tileLayer('http://localhost:8080/data/v3/#{z}/{x}/{y}', {
+  var mymap = map('map').setView([39.917241237249904, 116.397128], 13);
+  tileLayer('http://localhost:8080/styles/basic-preview/512/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
   }).addTo(mymap);
@@ -30,32 +23,11 @@ onMounted(()=>{
       // 你可以在这里添加代码来使用地理编码服务查询更多信息，比如建筑名称
   });
 })
-const name = ref('北京邮电大学')
-const res = ref('result')
-watch(name, ()=>{
-  axios.get(`https://photon.komoot.io/api/?q=${name}`)
-  .then(response => {
-    console.log(response);
-    return response.data
-  })
-  .then(data => {
-    // 假设我们只关心第一个搜索结果
-    var firstResult = data.features[0];
-    var placeName = firstResult.properties.name; // 地点名称
-    var coordinates = firstResult.geometry.coordinates; // 坐标，数组形式 [经度, 纬度]
-    // alert('地点名称:'+ name + coordinates);
-    res.value = `地点名称: ${placeName}, 坐标: ${coordinates.join(', ')}`; // 更新res的值
-    console.log(res)
-  })
-  .catch(error => console.log('请求失败', error));
-})
 </script>
 <template>
   <main>
     This is main page
     <div id="map"></div>
-    {{name}}:{{res}}
-    <input v-model="name">
   </main>
 </template>
 
@@ -64,7 +36,6 @@ body {
   margin: 0;
   padding: 0;
 }
-
 #map {
   height: 500px;
   width: 500px;

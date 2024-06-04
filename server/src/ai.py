0214c2ace9316c10eed2ef390b60a3dc5240818c
DrinkLessMilkTea <caiyiwen@bupt.edu.cn>
2024-06-05 00:14:27 +0800

    ✨ feat(src): ai

--- server/src/ai.py
+++ server/src/ai.py
@@ -5,19 +5,20 @@
 @return 游玩建议
 @return type str
 '''
+import time
 from zhipuai import ZhipuAI
 
 def get_suggestion(location:str):
     client = ZhipuAI(api_key="a9bae78a296e575676bb91c8b125bcfa.Q8G7UawOhN6yF2rl")
 
-    response = client.chat.completions.create(
+    response = client.chat.asyncCompletions.create(
         model="glm-4",
         messages=[
             {
                 "role":"user",
                 "content":f"我计划今天到{location}去游玩,请你给我一些游玩建议,包括介绍一下{location},今天的天气该穿什么衣服,有哪些推荐的参观点,以及推荐的参观路线"
             },
-        ],
+        ],# type:ignore
         max_tokens=8192,
         temperature=0.9,
         tools=[
@@ -31,7 +32,13 @@ def get_suggestion(location:str):
             },
         ],
     )
-    return response.choices[0].message.content # type:ignore
+    status = ''
+    task_id = response.id
+    while status != 'SUCCESS' and status != 'FAILED':
+        result_response = client.chat.asyncCompletions.retrieve_completion_result(id=task_id) # type:ignore
+        status = result_response.task_status
+        time.sleep(2)
+    return result_response.choices[0].message.content # type:ignore
 
 '''
 @brief 直接向大模型提问
@@ -40,18 +47,24 @@ def get_suggestion(location:str):
 @return 回答
 @return type str
 '''
-def get_response(question:str):
+def get_answer(question:str):
     client = ZhipuAI(api_key="a9bae78a296e575676bb91c8b125bcfa.Q8G7UawOhN6yF2rl")
 
-    response = client.chat.completions.create(
+    response = client.chat.asyncCompletions.create(
         model="glm-4",
         messages=[
             {
                 "role":"user",
-                "content":f"{question}"
+                "content":question
             },
-        ],
+        ],# type:ignore
         max_tokens=8192,
-        temperature=0.9,
+        temperature=0.9
     )
-    return response.choices[0].message.content # type:ignore
+    status = ''
+    task_id = response.id
+    while status != 'SUCCESS' and status != 'FAILED':
+        result_response = client.chat.asyncCompletions.retrieve_completion_result(id=task_id) # type:ignore
+        status = result_response.task_status
+        time.sleep(0.5)
+    return result_response.choices[0].message.content # type:ignore
